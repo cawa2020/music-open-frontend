a1f66bbc1be73e0bb5a278ae2384442ec6895fd3
leksbezdar <vollkovalex1991@mail.ru>
2024-05-16 15:08:53 +0400

    zen placeholder

--- src/app/core/services/api.service.ts
+++ src/app/core/services/api.service.ts
@@ -7,78 +7,101 @@ import { Playlist } from '../../shared/interfaces/playlist.interface';
 import { Method } from '../../shared/interfaces/app.interface';
 import { Song, SongBrief } from '../../shared/interfaces/song.interface';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
-
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getPlaylist(id: number) {
-    return this._request('GET', 'playlist/' + id)
+    return this._request('GET', 'playlist/' + id);
   }
 
   getArtist(id: number): Observable<Artist> {
-    return this._request('GET', 'artist/' + id)
+    return this._request('GET', 'artist/' + id);
   }
 
-  getArtistTop(id: number, limit?: number, index?: number): Observable<{ data: Song[], total: number }> {
-    let url = `artist/${id}/top?`
-    if (limit) url += 'limit=' + limit
-    if (index) url += '&index=' + index
-    return this._requestAPI('GET', url)
+  getArtistTop(
+    id: number,
+    limit?: number,
+    index?: number
+  ): Observable<{ data: Song[]; total: number }> {
+    let url = `artist/${id}/top?`;
+    if (limit) url += 'limit=' + limit;
+    if (index) url += '&index=' + index;
+    return this._requestAPI('GET', url);
   }
 
-  getArtistAlbums(id: number, limit?: number, index?: number): Observable<{ data: AlbumBrief[] }> {
-    let url = `artist/${id}/albums`
-    if (limit) { url += '?limit=' + limit }
-    if (index) { url += '?index=' + index }
-    return this._requestAPI('GET', url)
+  getArtistAlbums(
+    id: number,
+    limit?: number,
+    index?: number
+  ): Observable<{ data: AlbumBrief[] }> {
+    let url = `artist/${id}/albums`;
+    if (limit) {
+      url += '?limit=' + limit;
+    }
+    if (index) {
+      url += '?index=' + index;
+    }
+    return this._requestAPI('GET', url);
   }
 
   getArtistRelated(id: number, limit?: number): Observable<{ data: Artist[] }> {
-    let url = `artist/${id}/related`
-    if (limit) { url += '?limit=' + limit }
-    return this._requestAPI('GET', url)
+    let url = `artist/${id}/related`;
+    if (limit) {
+      url += '?limit=' + limit;
+    }
+    return this._requestAPI('GET', url);
   }
 
   getPlaylistsWithArtist(id: number): Observable<{ data: Playlist[] }> {
-    return this._requestAPI('GET', `artist/${id}/playlists`)
+    return this._requestAPI('GET', `artist/${id}/playlists`);
   }
 
   getAlbum(id: number) {
-    return this._request('GET', 'album/' + id)
+    return this._request('GET', 'album/' + id);
   }
 
   getAlbumTracks(id: number): Observable<{ data: SongBrief[] }> {
-    return this._requestAPI('GET', 'album/' + id + '/tracks')
+    return this._requestAPI('GET', 'album/' + id + '/tracks');
   }
 
   getBySearch(word: string): Observable<any> {
-    const path = 'search?q=' + word
-    return this._request('GET', path)
+    const path = 'search?q=' + word;
+    return this._request('GET', path);
   }
 
   private _request(method: Method, path: string, body?: any): Observable<any> {
-    let headers = new HttpHeaders()
-    headers = headers.set('X-RapidAPI-Key', 'b08fdc8c61msh2548eee4724e202p134b42jsn6bb48b026b0e')
-    headers = headers.set('X-RapidAPI-Host', 'deezerdevs-deezer.p.rapidapi.com')
+    let headers = new HttpHeaders();
+    headers = headers.set(
+      'X-RapidAPI-Key',
+      'b08fdc8c61msh2548eee4724e202p134b42jsn6bb48b026b0e'
+    );
+    headers = headers.set(
+      'X-RapidAPI-Host',
+      'deezerdevs-deezer.p.rapidapi.com'
+    );
 
     const options = {
       headers: headers,
-      body: body
-    }
+      body: body,
+    };
 
-    const url = 'https://deezerdevs-deezer.p.rapidapi.com/' + path
+    const url = 'https://deezerdevs-deezer.p.rapidapi.com/' + path;
 
-    return this.http.request(method, url, options)
+    return this.http.request(method, url, options);
   }
 
-  private _requestAPI(method: Method, path: string, body?: any): Observable<any> {
-    // const url = `https://thingproxy.freeboard.io/fetch/https://api.deezer.com/` + path
-    const url = `https://corsproxy.io/?https://api.deezer.com/` + path
+  private _requestAPI(
+    method: Method,
+    path: string,
+    body?: any
+  ): Observable<any> {
+    const url =
+      `https://thingproxy.freeboard.io/fetch/https://api.deezer.com/` + path;
+    // const url = `https://corsproxy.io/?https://api.deezer.com/` + path
 
-    return this.http.request(method, url)
+    return this.http.request(method, url);
   }
 }
